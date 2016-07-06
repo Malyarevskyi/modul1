4663df2f0ea5f22421257823165c69a4b1b6e76c
Malyarevskyi <pochta13a@gmail.com>
2016-07-06 17:28:49 +0300

    dz modul1
    home work

---
+++ GoItFirstProgram.java
@@ -0,0 +1,9 @@
+package com.goit.java.modul1;
+
+public class GoItFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+
+
+}
